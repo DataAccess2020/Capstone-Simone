fbe2aa9d04d746501143f2d76c0e8c007832c180
simonedeluca <delucasim@hotmail.it>
2021-02-08 19:47:55 +0100

    updated

--- 00_Looking_at.R
+++ 00_Looking_at.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(readxl)
 
 fanta_stat <- read_excel("Data/Statistiche_Fantacalcio_2020-21.xlsx")
+fanta_ratings <- read_excel("Data/Quotazioni_Fantacalcio.xlsx")
 
 head(fanta_stat)
 
@@ -13,8 +14,16 @@ fanta_stat <- fanta_stat[-1,]
 fanta_stat <- as.data.frame(fanta_stat)
 str(fanta_stat)
 
+colnames(fanta_ratings) <- as.character(fanta_ratings[1, ])
+fanta_ratings <- fanta_ratings[-1,]
+
+fanta_ratings <- as.data.frame(fanta_ratings)
+str(fanta_stat)
+
 #Convert data frame column to numeric type
 fanta_stat[, c(1,5:18)] <- sapply(fanta_stat[, c(1,5:18)], as.numeric)
+
+fanta_ratings[, c(1,5:7)] <- sapply(fanta_ratings[, c(1,5:7)], as.numeric)
 
 #Plotting
 
