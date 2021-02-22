34b029377e34125d1568da3981b1861619edea11
simonedeluca <77702910+simonedeluca@users.noreply.github.com>
2021-02-22 18:24:56 +0100

    Delete 01_Standing.R
    included in the next

--- 01_Standing.R
+++
@@ -1,8 +0,0 @@
-library(rvest)
-
-browseURL("https://www.fantacalcio.it/serie-a/classifica")
-
-standing <- read_html("https://www.fantacalcio.it/serie-a/classifica") %>%
-  html_nodes(css = 'table') %>%
-  html_table() %>%
-  .[[1]]
